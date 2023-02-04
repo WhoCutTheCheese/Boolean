7f7b5e7c6d701cddc68a2163ffa758e06827c84d
WhoCutTheCheese <aidenconway07@gmail.com>
2023-02-04 02:59:42 -0600

    amogus :sus:

--- commands/legacy/moderation/Nickname.ts
+++ commands/legacy/moderation/Nickname.ts
@@ -4,9 +4,7 @@ import {
   EmbedBuilder,
   Message,
   PermissionsBitField,
-  TextChannel,
 } from "discord.js";
-import Settings from "../../../schemas/Settings";
 import { Utilities } from "../../../utils/Utilities";
 import { EmbedUtils } from "../../../utils/EmbedUtils";
 
@@ -19,32 +17,23 @@ module.exports = {
   commandCategory: "Moderation",
   callback: async (client: Client, message: Message, args: string[]) => {
     if (!message.guild?.members?.me) {
-      message.channel.send({
-        content: "This command can only be used in a guild",
-      });
+      message.channel.send({ content: "This command can only be used in a guild", });
       return;
     }
 
     const settings = await new Utilities().getGuildSettings(message.guild!);
     if (!settings) return;
 
-    let color: ColorResolvable = await new Utilities().getEmbedColor(
-      message.guild!
-    );
+    let color: ColorResolvable = await new Utilities().getEmbedColor(message.guild!);
 
     const user =
       message.mentions.members?.first() ||
       (await message.guild?.members.fetch(args[0]).catch(() => {}));
     if (!user)
-      return message.channel.send({
-        content: "Unable to fetch that member! Please try again.",
-      });
+      return message.channel.send({ content: "Unable to fetch that member! Please try again.", });
 
     if (
-      message.guild?.ownerId === user.id ||
-      user.roles.highest.position >
-        message.guild.members.me.roles.highest.position
-    )
+      message.guild?.ownerId === user.id || user.roles.highest.position > message.guild.members.me.roles.highest.position)
       return message.channel.send({
         content: "I am unable to edit their username!",
       });
@@ -57,6 +46,7 @@ module.exports = {
       newNick = args.splice(1).join(" ");
       response = `You have set **${user.user.tag}**'s nickname to \`${newNick}\`!`;
       action = "Nickname Set";
+      if(newNick?.length > 30) { return message.channel.send({ content: "Nickname length must be less than 30 characters." }) }
     }
 
     let oldNick = user.user.username;
@@ -66,10 +56,7 @@ module.exports = {
 
     user.setNickname(newNick).catch((err: Error) => {
       console.error(err);
-      message.channel.send({
-        content:
-          "An unknwon error occurred! If this issue persists, please join our support server.",
-      });
+      message.channel.send({ content: "An unknwon error occurred! If this issue persists, please join our support server.", });
     });
 
     const nickSet = new EmbedBuilder().setDescription(response).setColor(color);
@@ -83,7 +70,7 @@ module.exports = {
         target: user,
         action: action,
       },
-      { title: action, actionInfo: `\`${oldNick}\` -> \`${newNick}\`` }
+      { title: "Nickname Edited", actionInfo: `\`${oldNick}\` -> \`${newNick}\`` }
     );
   },
 };
--- commands/legacy/moderation/Warn.ts
+++ commands/legacy/moderation/Warn.ts
@@ -1,11 +1,11 @@
-import {Client, ColorResolvable, Message} from "discord.js";
-import Settings from "../../../schemas/Settings";
+import {Client, ColorResolvable, Message, PermissionsBitField} from "discord.js";
 import { Utilities } from "../../../utils/Utilities";
 
 module.exports = {
     commands: ["warn", "warning", "infract"],
     minArgs: 1,
-    expectedArgs: "[@User/User ID] [Reason]",
+    expectedArgs: "[@User/User ID] (Reason)",
+    userPermissions: [ PermissionsBitField.Flags.ManageMessages ],
     commandCategory: "Moderation",
     callback: async (client: Client, message: Message, args: string[]) => {
 
@@ -15,6 +15,11 @@ module.exports = {
 
         const user = message.mentions.members?.first() || await message.guild?.members.fetch(args[0]).catch(() => {});
         if(!user) return message.channel.send({ content: "Unable to fetch that member! Please try again." });
+
+        let reason = "No reason provided.";
+        if(args[1]) {
+            reason = args.splice(1).join(" ");
+        }
 
     }
 }
