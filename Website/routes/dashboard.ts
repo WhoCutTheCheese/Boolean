2d4494ad3da96760b57791cb883b56b98839136f
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-02-06 03:44:48 -0600

    Dash

--- Website/routes/dashboard.ts
+++ Website/routes/dashboard.ts
@@ -8,7 +8,7 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 	let session: BooleanSession = req.session as BooleanSession
 	if (!session.passport || !session.passport.user) return res.redirect("/auth/login")
 
-	res.render('dashboard', session.passport.user)
+	res.render('dashboard', { servers: { name: "test", icon: "https://cdn.discordapp.com/icons/1071223504852238406/71087442d87b059a89c4839fed13e322.webp" } })
 });
 
 export default router;
