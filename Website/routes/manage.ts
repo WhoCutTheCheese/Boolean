fb1f0fbd822439cb05f217a0e50d7a6ca34a2a37
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-02-19 01:56:56 -0600

    Manage page additions & backend fixes

--- Website/routes/manage.ts
+++ Website/routes/manage.ts
@@ -24,36 +24,29 @@ router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
 
 	if (!dbserver || !server) return new Utilities().sendToInvite(req.params.id, req, res)
 
-	if (!getCooldown(user.discordId)) {
-		setCooldown(user.discordId);
+	console.log(dbserver)
 
-		let guilds = await new Utilities().updateGuilds(user.guilds);
-		if (guilds) {
-			(req.session as any).passport.user.guilds = guilds;
-			await WebUsers.findOneAndUpdate({ discordId: user.discordId }, { guilds: guilds });
-		}
-	}
-
-	console.log(userServer)
-
-	res.render('manage/main', { ...{ dbserver }, ...{ server }, ...{ config }, ...{ user }, ...{ userServer }, ...{ showminnavbar: true } })
+	res.render('manage/main', { ...{ config }, ...{ dbserver }, ...{ server }, ...{ user }, ...{ userServer }, ...{ showminnavbar: true } })
 });
 
 router.get('/:id/:module', async (req: Request, res: Response, next: NextFunction) => {
-
 	let session: BooleanSession = req.session as BooleanSession
 	if (!session.passport || !session.passport.user) return res.redirect("/")
 	let user = session.passport.user
 
-	let server = await ServerSchema.findOne({ guildID: req.params.id })
+	let dbserver = await ServerSchema.findOne({ guildID: req.params.id })
+	let server = await (await client.guilds.fetch(req.params.id)).toJSON()
+	let userServer = user.guilds.find(gld => gld.id === req.params.id)
+
+	if (!dbserver || !server) return new Utilities().sendToInvite(req.params.id, req, res)
+
 	let module = req.params.module.toLowerCase()
 
 	var viewPath = path.join(req.app.get('views'), "/manage", module);
 
-	if (!server) return res.redirect('/dashboard')
-
 	if (fs.existsSync(viewPath)) {
-		res.render(module, { ...{ config }, ...{ user }, ...{ server } });
+		// res.render(module, { ...{ config }, ...{ user }, ...{ server } });
+		res.render(module, { ...{ config }, ...{ dbserver }, ...{ server }, ...{ config }, ...{ user }, ...{ userServer }, ...{ showminnavbar: true } })
 	} else {
 		res.render('error', { ...{ config }, ...{ user } })
 	}
