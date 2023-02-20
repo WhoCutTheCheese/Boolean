e078e9640cf9fe2356ca5ada42490112537ac4e2
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-02-20 08:23:23 -0600

    Added start.bat & fixed reload cmd

--- Bot/commands/legacy/owner/Reload.ts
+++ Bot/commands/legacy/owner/Reload.ts
@@ -1,7 +1,6 @@
 import { Client, Message, TextChannel, Embed } from 'discord.js';
 import { BooleanCommand } from "../../../interface/BooleanCommand";
 import { EmbedUtils } from '../../../utils/EmbedUtils';
-import { Log, LogLevel } from '../../../utils/Log';
 import { Utilities } from '../../../utils/Utilities';
 
 const command: BooleanCommand = {
@@ -21,7 +20,7 @@ const command: BooleanCommand = {
 		const commandName = args[0].trim().toLowerCase();
 		let commandpath = client.legacycommandfilepath.get(commandName)! || client.legacycommandfilepath.get(client.legacycommandalias.get(commandName)!)!
 		if (!commandpath) return new EmbedUtils().sendErrorEmbed((message.channel as TextChannel), message, { errorEmoji: false, replyToMessage: true, deleteMsg: true }, { title: "Unknown command", description: "That command does not exist" })
-		let command = client.legacycommands.get(commandName)! || client.legacycommandfilepath.get(client.legacycommandalias.get(commandName)!)!
+		let command = client.legacycommands.get(commandName)! || client.legacycommands.get(client.legacycommandalias.get(commandName)!)!
 
 		let loaded = await new Utilities().loadCommand(client, commandpath)
 		if (loaded) return new EmbedUtils().sendSuccessEmbed((message.channel as TextChannel), message, { successEmoji: true, replyToMessage: true }, { description: `Successfully reloaded \`${command.command}\`` })
---
+++ Bot/start.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+:start
+cls
+TITLE Boolean Bot
+ts-node index.ts
+goto start
