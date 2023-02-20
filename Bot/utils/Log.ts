4413f764feae19f4f336e49f46a4c08b8f908398
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-02-20 02:33:31 -0600

    Format log.ts

--- Bot/utils/Log.ts
+++ Bot/utils/Log.ts
@@ -7,14 +7,14 @@ import { Utilities } from "./Utilities";
  * @enum {number}
  */
 export enum LogLevel {
-    /** Information severity. */
-    Info,
-    /** Debugging severity. */
-    Debug,
-    /** Code warning severity. */
-    Warn,
-    /** Code error severity. */
-    Error,
+	/** Information severity. */
+	Info,
+	/** Debugging severity. */
+	Debug,
+	/** Code warning severity. */
+	Warn,
+	/** Code error severity. */
+	Error,
 }
 
 /**
@@ -24,23 +24,23 @@ export enum LogLevel {
  */
 export async function Log(level: LogLevel, message: string): Promise<void> {
 
-    let levelMessage = "";
+	let levelMessage = "";
 
-    switch (level) {
-        case (LogLevel.Info): levelMessage = "[INFO]".magenta; break;
-        case (LogLevel.Debug): levelMessage = "[DEBUG]".blue; break;
-        case (LogLevel.Warn): levelMessage = "[WARNING]".yellow; break;
-        case (LogLevel.Debug): levelMessage = "[ERROR]".red; break;
-    }
-    if (levelMessage == undefined || levelMessage == null) {
-        levelMessage = "Peinisi"
-    }
+	switch (level) {
+		case (LogLevel.Info): levelMessage = "[INFO]".magenta; break;
+		case (LogLevel.Debug): levelMessage = "[DEBUG]".blue; break;
+		case (LogLevel.Warn): levelMessage = "[WARNING]".yellow; break;
+		case (LogLevel.Error): levelMessage = "[ERROR]".red; break;
+	}
+	if (levelMessage == undefined || levelMessage == null) {
+		levelMessage = ""
+	}
 
-    let awd = `${await timeStringNow()} ${levelMessage} | ${message}`
-    console.log(awd)
+	let awd = `${await timeStringNow()} ${levelMessage} | ${message}`
+	console.log(awd)
 
 }
 async function timeStringNow(): Promise<string> {
-    const now = new Date();
-    return `${now.getUTCDate().toString().padStart(2, "0")}-${(now.getUTCMonth() + 1).toString().padStart(2, "0")}-${now.getUTCFullYear().toString().padStart(4, "0")} ${now.getUTCHours().toString().padStart(2, "0")}:${now.getUTCMinutes().toString().padStart(2, "0")}:${now.getUTCSeconds().toString().padStart(2, "0")}:${now.getUTCMilliseconds().toString().padStart(3, "0")}`;
+	const now = new Date();
+	return `${now.getUTCDate().toString().padStart(2, "0")}-${(now.getUTCMonth() + 1).toString().padStart(2, "0")}-${now.getUTCFullYear().toString().padStart(4, "0")} ${now.getUTCHours().toString().padStart(2, "0")}:${now.getUTCMinutes().toString().padStart(2, "0")}:${now.getUTCSeconds().toString().padStart(2, "0")}:${now.getUTCMilliseconds().toString().padStart(3, "0")}`;
 }
