dc6810cec8ac75ede8675eba48b218e89f22448d
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-02-20 15:14:09 -0600

    Added logging to reload

--- Bot/commands/legacy/owner/Reload.ts
+++ Bot/commands/legacy/owner/Reload.ts
@@ -1,6 +1,7 @@
 import { Client, Message, TextChannel, Embed } from 'discord.js';
 import { BooleanCommand } from "../../../interface/BooleanCommand";
 import { EmbedUtils } from '../../../utils/EmbedUtils';
+import { Log, LogLevel } from '../../../utils/Log';
 import { Utilities } from '../../../utils/Utilities';
 
 const command: BooleanCommand = {
@@ -13,18 +14,25 @@ const command: BooleanCommand = {
 	expectedArgs: "(cmd/alias)",
 	devOnly: true,
 	callback: async (client: Client, message: Message, args: string[]) => {
-		if (args.length === 0) {
-			return new EmbedUtils().sendArgsErrorEmbed(message, 1, exports)
-		}
+		if (args.length === 0) return new EmbedUtils().sendArgsErrorEmbed(message, 1, exports)
 
 		const commandName = args[0].trim().toLowerCase();
 		let commandpath = client.legacycommandfilepath.get(commandName)! || client.legacycommandfilepath.get(client.legacycommandalias.get(commandName)!)!
 		if (!commandpath) return new EmbedUtils().sendErrorEmbed((message.channel as TextChannel), message, { errorEmoji: false, replyToMessage: true, deleteMsg: true }, { title: "Unknown command", description: "That command does not exist" })
 		let command = client.legacycommands.get(commandName)! || client.legacycommands.get(client.legacycommandalias.get(commandName)!)!
 
+		Log(LogLevel.Info, `[Loading] | Legacy Command | ${command.command}`)
+
 		let loaded = await new Utilities().loadCommand(client, commandpath)
-		if (loaded) return new EmbedUtils().sendSuccessEmbed((message.channel as TextChannel), message, { successEmoji: true, replyToMessage: true }, { description: `Successfully reloaded \`${command.command}\`` })
-		else return new EmbedUtils().sendErrorEmbed((message.channel as TextChannel), message, { errorEmoji: true, replyToMessage: true, deleteMsg: true }, { description: `There was an error reloading \`${command.command}\`` })
+
+		if (loaded) {
+			Log(LogLevel.Info, `[Loaded]  | Legacy Command | ${command.command}`)
+			return new EmbedUtils().sendSuccessEmbed((message.channel as TextChannel), message, { successEmoji: true, replyToMessage: true }, { description: `Successfully reloaded \`${command.command}\`` })
+		}
+		else {
+			Log(LogLevel.Error, `There was an error loading ${command.command}`)
+			return new EmbedUtils().sendErrorEmbed((message.channel as TextChannel), message, { errorEmoji: true, replyToMessage: true, deleteMsg: true }, { description: `There was an error reloading \`${command.command}\`` })
+		}
 	},
 }
 
