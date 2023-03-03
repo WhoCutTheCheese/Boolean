587993241ef94c88f4e169770f141c56bf577008
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2023-03-03 17:00:38 -0600

    Stupid shit

--- Website/bot.ts
+++ Website/bot.ts
@@ -1,7 +1,8 @@
 import { GatewayIntentBits, Client } from "discord.js";
 import dotENV from "dotenv";
 dotENV.config();
-const token = process.env.token;
+const token = process.env.token || process.env.beta_token;
+// const token = process.env.token;
 
 const client = new Client({
 	intents: [
